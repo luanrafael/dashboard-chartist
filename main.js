64a2cf86e593184413006aa31ce79d8153a16d2e
Luan Rafael C. Pinheiro <luan-g8@hotmail.com>
2016-09-28 15:02:36 -0300

    adicionando drag and drop e criando cards

--- main.js
+++ main.js
@@ -32,9 +32,6 @@ var Dashboard = function (){
 			
 			chart.on('draw', function(context) {
 				var value = Math.floor( Math.random() * 1000);
-				console.log(value);
-				console.log(context);
-				console.log(context.type);
 				if(context.type == 'label') {
 					context.element.attr({
 			    		style: 'stroke: #fff;'
@@ -64,7 +61,7 @@ var Dashboard = function (){
 			widget.appendChild(remove_span);
 
 			widget.setAttribute('id', "widget_" + Date.now());
-
+			widget.setAttribute('draggable', true);
 
 			return widget;
 
@@ -75,10 +72,9 @@ var Dashboard = function (){
 			var id = widget.getAttribute('id');
 			var selector = '#' + id + ' .remove';
 
-			$(selector).addEventListener('click', function(){
-				if(!confirm('Deseja realmente excluir o widget? ')) return;
-				this.parentNode.remove();
-			});
+			add_click($(selector));
+			allow_drag_drop($('#' + id));
+			
 		}
 
 
@@ -112,16 +108,75 @@ window.onload = function() {
 function refresh_events(){
 
 	$$('.widget .remove').forEach(function(element) {
-		element.addEventListener('click', function(){
-			if(!confirm('Deseja realmente excluir o widget? ')) return;
-			this.parentNode.remove()
-		});
+		add_click(element);
 	});
 
 }
 
+function add_click(element) {
+	element.addEventListener('click', remove_widget, false);
+}
 
+function allow_drag_drop (element) {
+	element.addEventListener('dragstart', handleDragStart, false);
+  	element.addEventListener('dragenter', handleDragEnter, false);
+	element.addEventListener('dragover', handleDragOver, false);
+	element.addEventListener('dragleave', handleDragLeave, false);
+	element.addEventListener('drop', handleDrop, false);
+	element.addEventListener('dragend', handleDragEnd, false);
+}
 
+function remove_widget(){
+	if(!confirm('Deseja realmente excluir o widget? ')) return;
+	this.parentNode.remove()
+}
 
+var dragSrcEl;
 
+function handleDragStart(e) {
+	this.style.opacity = '0.4';  // this / e.target is the source node.
 
+	dragSrcEl = this;
+
+	e.dataTransfer.effectAllowed = 'move';
+	e.dataTransfer.setData('text/html', this.innerHTML);
+}
+
+function handleDragOver(e) {
+	if (e.preventDefault) {
+		e.preventDefault(); // Necessary. Allows us to drop.
+	}
+
+	e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+	this.style.opacity = '1';
+	return false;
+}
+
+function handleDragEnter(e) {
+	e.dataTransfer.dropEffect = 'move';
+	this.classList.add('over');
+}
+
+function handleDragLeave(e) {
+	this.classList.remove('over');
+}
+
+function handleDrop(e) {
+	
+	if (e.stopPropagation) {
+		e.stopPropagation();
+  	}
+
+	if (dragSrcEl != this) {
+		dragSrcEl.innerHTML = this.innerHTML;
+	    this.innerHTML = e.dataTransfer.getData('text/html');
+	}
+
+  	return false;
+}
+
+function handleDragEnd(e) {
+	[].forEach.call($$('.widget'), function (col) {
+		col.classList.remove('over');
+  	});
+}
